a10bc87621ee28e7355f562fca1d38eb29f124a6
cironis <ciroshia@gmail.com>
2024-08-28 20:34:08 +0000

    Updating app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+#######################
+# Import libraries
+import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
+
+#######################
+# Page configuration
+st.set_page_config(
+    page_title="US Population Dashboard",
+    page_icon="🏂",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
