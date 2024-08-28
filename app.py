6431bd7d26c72fb11cd86c4f93796765fcd5e0ec
cironis <ciroshia@gmail.com>
2024-08-28 21:01:45 +0000

    Updating app.py

--- app.py
+++ app.py
@@ -8,13 +8,16 @@ import plotly.express as px
 #######################
 # Page configuration
 st.set_page_config(
-    page_title="☘️ Sorteador",
+    page_title="Sorteador",
     page_icon="☘️",
     layout="wide",
     initial_sidebar_state="expanded")
 
 #######################
 # Title
-st.title("Sorteador")
-title = st.text_input("Movie title", "Life of Brian")
-st.write("The current movie title is", title)
+st.title("☘️ Sorteador")
+
+def teste_write():
+    st.write("The current movie title is", title)
+
+title = st.text_input("Movie title", on_change=teste_write)
