d8ab05089bafcd5120ce5b6b546648247fe38171
cironis <ciroshia@gmail.com>
2024-09-12 02:33:34 +0000

    Updating app.py

--- views/boardgame_list.py
+++ views/boardgame_list.py
@@ -5,11 +5,15 @@ import pandas as pd
 
 st.set_page_config(page_title="Board Game - Database", page_icon="♙",layout="wide")
 
-st.title("Board Game - Database")
-
 @st.cache_data
 def load_main_dataframe(worksheet):
 
     conn = st.connection("gsheets", type=GSheetsConnection)
     df = conn.read(worksheet=worksheet)
     return df
+
+st.title("Board Game - Database")
+
+df = load_main_dataframe("database")
+
+st.dataframe(df)
