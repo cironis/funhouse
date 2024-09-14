a122b3de207fe905e168535006ff954601df4acd
cironis <ciroshia@gmail.com>
2024-09-14 18:29:19 +0000

    Updating app.py

--- views/boardgame_list.py
+++ views/boardgame_list.py
@@ -16,39 +16,38 @@ st.title("Board Game - Database")
 
 df = load_main_dataframe("database")
 
-# Define the cell renderer to display image as a hyperlink
-cell_renderer = JsCode('''
-function(params) {
-    return `<a href="${params.data.url}" target="_blank">
-                <img src="${params.data.thumbnail}" style="height:60px;">
-            </a>`;
-}
-''')
-
-# Define the function to set row height
-get_row_height = JsCode('''
-function(params) {
-    return 80;
-}
-''')
-
-# Build grid options
+# Configure the 'thumbnail' column to use 'agImageCellRenderer'
 gb = GridOptionsBuilder.from_dataframe(df)
-gb.configure_column('thumbnail', headerName='Image', cellRenderer=cell_renderer, html=True)  # Added html=True
+gb.configure_column(
+    'thumbnail',
+    headerName='Image',
+    cellRenderer='agImageCellRenderer',
+    width=100
+)
 gb.configure_column('name', headerName='Name')
 gb.configure_column('year', headerName='Year')
 gb.configure_column('minplayers', headerName='Min Players')
 gb.configure_column('maxplayers', headerName='Max Players')
 gb.configure_columns(['playingtime', 'url'], hide=True)
-gb.configure_grid_options(getRowHeight=get_row_height)
+gb.configure_grid_options(
+    getRowHeight=JsCode('function(params) { return 80; }')
+)
+
+# Define the onCellClicked event
+on_cell_clicked = JsCode('''
+function(event) {
+    if (event.colDef.field === 'thumbnail') {
+        window.open(event.data.url, '_blank');
+    }
+}
+''')
 
 gridOptions = gb.build()
+gridOptions['onCellClicked'] = on_cell_clicked
 
 # Display the grid
 AgGrid(
     df,
     gridOptions=gridOptions,
-    allow_unsafe_jscode=True,
-    unsafe_allow_html=True  # Added unsafe_allow_html=True
+    allow_unsafe_jscode=True
 )
-
