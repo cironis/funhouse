d732d59deb116f667258579690ad30fcb6712694
cironis <ciroshia@gmail.com>
2024-09-14 19:47:02 +0000

    Updating app.py

--- views/boardgame_list.py
+++ views/boardgame_list.py
@@ -22,7 +22,7 @@ cell_renderer =  JsCode("""
             this.eGui = document.createElement('img');
             this.eGui.setAttribute('src', params.value);
             this.eGui.setAttribute('width', "100");
-            this.eGui.setAttribute('height', "150");
+            this.eGui.setAttribute('height', "200");
           }
           getGui() {
             return this.eGui;
