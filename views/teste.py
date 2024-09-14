bd06ce2d8a8b901ff97f7cf1cb7c0bb7d7186779
cironis <ciroshia@gmail.com>
2024-09-14 19:43:21 +0000

    Updating app.py

--- views/teste.py
+++ views/teste.py
@@ -18,7 +18,7 @@ df = pd.DataFrame(data)
 gb = GridOptionsBuilder.from_dataframe(df,
                                         editable=True)
 
-gb.configure_grid_options(Height=None)
+gb.configure_grid_options(rowHeight=100)
 
 cell_renderer =  JsCode("""
         class UrlCellRenderer {
