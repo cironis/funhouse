6229262cd737c0ca2c9c4f3e99c70a601e8823b4
cironis <ciroshia@gmail.com>
2024-09-15 15:29:44 +0000

    Updating app.py

---
+++ aux/aggrid_builder.py
@@ -0,0 +1,96 @@
+
+import streamlit as st
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
+from st_aggrid.shared import JsCode
+
+def create_grid(df):
+    
+    cell_renderer = JsCode("""
+        class UrlCellRenderer {
+            init(params) {
+                // Create an anchor element
+                this.eGui = document.createElement('a');
+                this.eGui.href = params.data.url; // Set the href to the 'url' field in your data
+                this.eGui.target = '_blank'; // Optional: Open link in a new tab
+
+                // Create an image element
+                var img = document.createElement('img');
+                img.src = params.value; // Use the cell value as the image source
+                img.height = 100;
+                img.style.display = 'block';
+                img.style.marginLeft = 'auto';
+                img.style.marginRight = 'auto';
+
+                // Append the image to the anchor
+                this.eGui.appendChild(img);
+            }
+
+            getGui() {
+                return this.eGui;
+            }
+        }
+    """)
+
+
+
+    # Configure the 'thumbnail' column to use the built-in image renderer with correct parameters
+    gb = GridOptionsBuilder.from_dataframe(df)
+
+    column_defs = [
+        {
+            'field': 'thumbnail',
+            'headerName': 'Image',
+            'cellRenderer': cell_renderer
+        },
+        {
+            'field': 'name',
+            'headerName': 'Name',
+            'flex': 2
+        },
+        {
+            'field': 'year',
+            'headerName': 'Year'
+        },
+        {
+            'field': 'minplayers',
+            'headerName': 'Min Players'
+        },
+        {
+            'field': 'maxplayers',
+            'headerName': 'Max Players'
+        },
+        {
+            'field': 'playingtime',
+            'headerName': 'Playing Time (min)'
+        },
+        {
+            'field': 'url',
+            'headerName': 'URL',
+            'hide': True  # Hide the 'url' column but keep it in data
+        }
+    ]
+
+    # Build the grid options
+    gridOptions = {
+        'columnDefs': column_defs,
+        'rowHeight': 120
+    }
+
+    on_grid_ready = JsCode("""
+    function(event) {
+        event.api.sizeColumnsToFit();
+    }
+    """)
+
+    gridOptions['onGridReady'] = on_grid_ready
+
+
+    grid = AgGrid(
+        df,
+        gridOptions=gridOptions,
+        allow_unsafe_jscode=True,
+        fit_columns_on_grid_load=True,
+        theme="material"
+    )
+
+    return grid
--- views/boardgame_list.py
+++ views/boardgame_list.py
@@ -3,6 +3,7 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 from st_aggrid import AgGrid, GridOptionsBuilder, JsCode
+from aux.aggrid_builder import create_grid
 
 st.set_page_config(page_title="Board Game - Database", page_icon="♙", layout="wide")
 
@@ -21,7 +22,7 @@ filter_1,filter_2 = st.columns(2)
 
 with filter_1:
     num_players = st.number_input("Number of players",step=1,min_value=1,placeholder="Number of players",value=2)
-    
+
 with filter_2:
     diy_filter = st.selectbox("DIY",["Todos","Sim","Não"])
 
@@ -34,92 +35,94 @@ if diy_filter != "Todos":
     else:
         df = df[df['DIY'] == False]
 
+grid = create_grid(df)
+grid
 
-cell_renderer = JsCode("""
-    class UrlCellRenderer {
-        init(params) {
-            // Create an anchor element
-            this.eGui = document.createElement('a');
-            this.eGui.href = params.data.url; // Set the href to the 'url' field in your data
-            this.eGui.target = '_blank'; // Optional: Open link in a new tab
+# cell_renderer = JsCode("""
+#     class UrlCellRenderer {
+#         init(params) {
+#             // Create an anchor element
+#             this.eGui = document.createElement('a');
+#             this.eGui.href = params.data.url; // Set the href to the 'url' field in your data
+#             this.eGui.target = '_blank'; // Optional: Open link in a new tab
 
-            // Create an image element
-            var img = document.createElement('img');
-            img.src = params.value; // Use the cell value as the image source
-            img.height = 100;
-            img.style.display = 'block';
-            img.style.marginLeft = 'auto';
-            img.style.marginRight = 'auto';
+#             // Create an image element
+#             var img = document.createElement('img');
+#             img.src = params.value; // Use the cell value as the image source
+#             img.height = 100;
+#             img.style.display = 'block';
+#             img.style.marginLeft = 'auto';
+#             img.style.marginRight = 'auto';
 
-            // Append the image to the anchor
-            this.eGui.appendChild(img);
-        }
+#             // Append the image to the anchor
+#             this.eGui.appendChild(img);
+#         }
 
-        getGui() {
-            return this.eGui;
-        }
-    }
-""")
-
-
-
-# Configure the 'thumbnail' column to use the built-in image renderer with correct parameters
-gb = GridOptionsBuilder.from_dataframe(df)
-
-column_defs = [
-    {
-        'field': 'thumbnail',
-        'headerName': 'Image',
-        'cellRenderer': cell_renderer
-    },
-    {
-        'field': 'name',
-        'headerName': 'Name',
-        'flex': 2
-    },
-    {
-        'field': 'year',
-        'headerName': 'Year'
-    },
-    {
-        'field': 'minplayers',
-        'headerName': 'Min Players'
-    },
-    {
-        'field': 'maxplayers',
-        'headerName': 'Max Players'
-    },
-    {
-        'field': 'playingtime',
-        'headerName': 'Playing Time (min)'
-    },
-    {
-        'field': 'url',
-        'headerName': 'URL',
-        'hide': True  # Hide the 'url' column but keep it in data
-    }
-]
-
-# Build the grid options
-gridOptions = {
-    'columnDefs': column_defs,
-    'rowHeight': 120
-}
-
-on_grid_ready = JsCode("""
-function(event) {
-    event.api.sizeColumnsToFit();
-}
-""")
-
-gridOptions['onGridReady'] = on_grid_ready
+#         getGui() {
+#             return this.eGui;
+#         }
+#     }
+# """)
 
 
-AgGrid(
-    df,
-    gridOptions=gridOptions,
-    allow_unsafe_jscode=True,
-    fit_columns_on_grid_load=True,
-    theme="material"
-)
+
+# # Configure the 'thumbnail' column to use the built-in image renderer with correct parameters
+# gb = GridOptionsBuilder.from_dataframe(df)
+
+# column_defs = [
+#     {
+#         'field': 'thumbnail',
+#         'headerName': 'Image',
+#         'cellRenderer': cell_renderer
+#     },
+#     {
+#         'field': 'name',
+#         'headerName': 'Name',
+#         'flex': 2
+#     },
+#     {
+#         'field': 'year',
+#         'headerName': 'Year'
+#     },
+#     {
+#         'field': 'minplayers',
+#         'headerName': 'Min Players'
+#     },
+#     {
+#         'field': 'maxplayers',
+#         'headerName': 'Max Players'
+#     },
+#     {
+#         'field': 'playingtime',
+#         'headerName': 'Playing Time (min)'
+#     },
+#     {
+#         'field': 'url',
+#         'headerName': 'URL',
+#         'hide': True  # Hide the 'url' column but keep it in data
+#     }
+# ]
+
+# # Build the grid options
+# gridOptions = {
+#     'columnDefs': column_defs,
+#     'rowHeight': 120
+# }
+
+# on_grid_ready = JsCode("""
+# function(event) {
+#     event.api.sizeColumnsToFit();
+# }
+# """)
+
+# gridOptions['onGridReady'] = on_grid_ready
+
+
+# AgGrid(
+#     df,
+#     gridOptions=gridOptions,
+#     allow_unsafe_jscode=True,
+#     fit_columns_on_grid_load=True,
+#     theme="material"
+# )
 
